66057ac3e7f78461f4149479ed3ac0a42f45ffa3
Jeffrey Moulckers <jrmoulckers@gmail.com>
2025-03-22 18:39:19 -0700

    Switched main layout of images to grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,8 +9,8 @@ async function Images() {
   const images = await getMyImages();
 
   return (
-    <div className="flex flex-wrap gap-4">
-      {images.map((image, index) => (
+    <div className="flex flex-wrap justify-center gap-4 p-4">
+      {[...images, ...images, ...images, ...images].map((image, index) => (
         <div key={image.id} className="w-48">
           <Link href={`/img/${image?.id}`}>
             <Image
